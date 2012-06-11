b1d568c10498c69e290c99ca5cf9892228515778
Michael Granados <michaelgranados@gmail.com>
2012-06-11 10:44:08 -0300

    Creating a checkbox type

--- src/Template/Default.php
+++ src/Template/Default.php
@@ -4,8 +4,9 @@ class Apolo_Component_Formulator_Template_Default
     extends Apolo_Component_Formulator_Template
 {
     public $templates = array(
-        'html'  => '{content}',
-        'input' => '<label><span>{label.name}</span> <input{input.attrs!} /></label>'
+        'html'     => '{content}',
+        'checkbox' => '<label><input{input.attrs!} /><span>{label.name}</span></label>',
+        'input'    => '<label><span>{label.name}</span> <input{input.attrs!} /></label>',
     );
 
     public function decorator($output)

65e1eb0612e964793adba246ae6d690c6bc6feee
Rainer Eduardo Lopez <rainer@corp.virgula.com.br>
2012-06-11 15:06:07 -0300

    Creating a File input ticket #156

---
+++ src/Element/Input/File.php
@@ -0,0 +1,20 @@
+<?php
+
+$ds   = DIRECTORY_SEPARATOR;
+$file = realpath(__DIR__ . $ds . '..' . $ds . 'Input.php');
+require_once $file;
+unset($ds, $file);
+
+class Apolo_Component_Formulator_Element_Input_File
+    extends Apolo_Component_Formulator_Element_Input
+    implements Apolo_Component_Formulator_ElementInterface
+{
+    public    $templateType = 'input';
+    protected $needsLabel   = true;
+    protected $type         = 'file';
+
+    public $validAttributes    = array(
+        'label' => array('name'),
+        'input' => array('attrs', '_type', 'name', 'id', 'disabled', 'accept', 'multiple'),
+    );
+}

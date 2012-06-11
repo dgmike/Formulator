9152a1257ece98ae6b2f7285b9ec03ab03fb1939
Rainer Eduardo Lopez <rainer@corp.virgula.com.br>
2012-06-11 12:38:37 -0300

    Creating a Email input ticket #156

---
+++ src/Element/Input/Email.php
@@ -0,0 +1,20 @@
+<?php
+
+$ds   = DIRECTORY_SEPARATOR;
+$file = realpath(__DIR__ . $ds . '..' . $ds . 'Input.php');
+require_once $file;
+unset($ds, $file);
+
+class Apolo_Component_Formulator_Element_Input_Email
+    extends Apolo_Component_Formulator_Element_Input
+    implements Apolo_Component_Formulator_ElementInterface
+{
+    public    $templateType = 'input';
+    protected $needsLabel   = true;
+    protected $type         = 'email';
+
+    public $validAttributes    = array(
+        'label' => array('name'),
+        'input' => array('attrs', '_type', 'name', 'value', 'id', 'placeholder', 'readonly', 'disabled'),
+    );
+}

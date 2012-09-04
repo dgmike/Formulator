4978200b2b25933acb4b8e4a4f72808805efa7ba
Michael Granados <michaelgranados@gmail.com>
2012-09-04 15:38:38 -0300

    Creating test for attributes in select element.

--- tests/Element/SelectTest.php
+++ tests/Element/SelectTest.php
@@ -59,4 +59,36 @@ class Apolo_Component_Formulator_Element_SelectTest
                   . '</select>';
         $this->assertEquals($expected, $this->form->render('elements'));
     }
+    
+    public function testCreateWithSomeAttributes()
+    {
+        $this->form->addElement(array(
+            'type'      => 'select',
+            'name'      => 'os',
+            'multiple'  => 'multiple',
+            'size'      => 5,
+            'autofocus' => 'autofocus',
+            'values'    => array(
+                'win'   => 'Windows',
+                'linux' => 'Linux',
+                'mac'   => 'Mac Os',
+            )
+        ));
+        $expected = '<label>' . PHP_EOL
+                  . '    <span>' . PHP_EOL
+                  . '    </span>' . PHP_EOL
+                  . '    <select name="os" multiple="multiple" size="5" autofocus="autofocus">' . PHP_EOL
+                  . '        <option value="win">' . PHP_EOL 
+                  . '            Windows' . PHP_EOL
+                  . '        </option>' . PHP_EOL
+                  . '        <option value="linux">' . PHP_EOL
+                  . '            Linux' . PHP_EOL
+                  . '        </option>' . PHP_EOL
+                  . '        <option value="mac">' . PHP_EOL
+                  . '            Mac Os' . PHP_EOL
+                  . '        </option>' . PHP_EOL
+                  . '    </select>' . PHP_EOL
+                  . '</label>';
+        $this->assertEquals($expected, $this->form->render('elements'));
+    }
 }
